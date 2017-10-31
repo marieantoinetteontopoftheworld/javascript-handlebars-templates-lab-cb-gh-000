290b4e9c1857e4e4d126e994b206f3612f94e391
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-31 12:26:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function createRecipe() {
   var template = Handlebars.compile(document.getElementById('recipe-template').innerHTML);
   var result = template(data);
 
-  document.getElementById('main') += result;
+  document.getElementsByTagName('main')[0] += result;
 }
 
 function init() {
