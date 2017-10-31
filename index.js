0a842117c95a24ab15b1c41eae912408235f9240
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-31 15:10:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,6 +32,25 @@ function displayEditForm() {
   document.getElementsByTagName('main')[0].innerHTML += result;
 }
 
+function updateRecipe() {
+  data['description'] = document.getElementsByName('edit-details')[0].value;
+
+  var ingredientsNodes = document.getElementsByName('edit-ingredient');
+  var ingredients = [];
+  for (i = 0; i < ingredientsNodes.length; i++) {
+    if (ingredientsNodes[i].value) {
+      ingredients[i] = ingredientsNodes[i].value;
+    }
+  }
+
+  data['ingredients'] = ingredients;
+
+  var template = Handlebars.compile(document.getElementById('recipe-template').innerHTML);
+  var result = template(data);
+
+  document.getElementById('recipe').innerHTML = result;
+}
+
 function init() {
   //put any page initialization/handlebars initialization here
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById('recipe-details-partial').innerHTML);
