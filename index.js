ecdde5face4d65cd86ac37dd4fb4e16139ab7fde
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-31 11:28:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+function createRecipe() {
+  var name = document.forms['recipe-form'].elements[0];
+  alert(name);
+}
+
 function init() {
   //put any page initialization/handlebars initialization here
+
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
